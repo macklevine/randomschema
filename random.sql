eab2e09bcb70bedcee0d8f13d3f380fa0075aed1
Mack Levine <mackplevine@gmail.com>
2015-07-26 23:45:28 -0700

    included instructions for how to instantiate schema using sequelize.

--- random.sql
+++ random.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
@@ -19,6 +19,28 @@ CREATE TABLE users (
   PRIMARY KEY (ID)
 );
 
+------------------------------------Example ORM Below---------------------------------
+
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("chat", "root", "");
+
+var Message = sequelize.define('Message', {
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
+
+var User = sequelize.define('User', {
+  username: Sequelize.STRING
+});
+
+Message.belongsTo(User);
+User.hasMany(Message);
+
+User.sync();
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
 /* select messages.id, messages.text, messages.roomname, users.username from messages
 * left outer join users on (messages.userid = users.id) order by messages.id desc
 * [a query string to select all messages]
